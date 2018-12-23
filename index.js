2a982c1434eb6983e52b22b5d5c638ab17f1fadb
Eyal Toledano <eyaltoledano@me.com>
2018-12-23 15:54:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var name = "Joe"
 var height = "74"
 
 if (name === "Susan") {
-  console.console.log("The name is correct.");
+  console.log("The name is correct.");
 }
+
+
 
 // Don't worry about this
 module.exports = { name, height
 }
-
